773227bef32c5af3df5829ddd41fc2c944100e6e
Emily W. <emilywang188@gmail.com>
2023-10-11 23:45:48 -0400

    test: finished load tests hopefully

--- tests/mock-load.test.ts
+++ tests/mock-load.test.ts
@@ -20,12 +20,10 @@ test("load pizza.csv", async ({ page }) => {
 });
 
 /**
- * Tests loading an empty csv.
+ * Tests loading an empty csv, and mode change
  */
 test("load poke.csv", async ({ page }) => {
-  await page
-    .getByPlaceholder("Enter command here!")
-    .fill("load_file poke.csv");
+  await page.getByPlaceholder("Enter command here!").fill("load_file poke.csv");
   await page.getByRole("button").click();
   await(
     expect(page.getByTestId("output")).toHaveText(
@@ -33,10 +31,19 @@ test("load poke.csv", async ({ page }) => {
     )
   );
   await(expect(page.getByTestId("output")).not.toContainText("Command: load_file poke.csv"));
+  
+  await page.getByPlaceholder("Enter command here!").fill("mode");
+  await page.getByRole("button").click();
+  await(
+    expect(page.getByTestId("output")).toHaveText(
+      "Command:load_file poke.csv Result:poke.csv loaded successfully!Command:mode" + 
+      " Result:Mode changed!Enter a command: Submitted 2 timesCurrent mode: verbose"
+    )
+  );
 });
 
 /**
- * Loads multiple csv files successfully
+ * Loads multiple csv files successfully, and mode change
  */
 test("load multiple csv", async ({ page }) => {
   await page.getByPlaceholder("Enter command here!").fill("load_file poke.csv");
@@ -50,26 +57,51 @@ test("load multiple csv", async ({ page }) => {
   await expect(page.getByTestId("output")).toHaveText(
     "poke.csv loaded successfully!pizza.csv loaded successfully!Enter a command: Submitted 2 timesCurrent mode: brief"
   );
+
+  await page.getByPlaceholder("Enter command here!").fill("mode");
+  await page.getByRole("button").click();
+  await(
+    expect(page.getByTestId("output")).toHaveText("Command:load_file poke.csv Result:poke.csv loaded successfully!" + 
+    "Command:load_file pizza.csv Result:pizza.csv loaded successfully!Command:mode Result:Mode changed!"+
+    "Enter a command: Submitted 3 timesCurrent mode: verbose"
+    )
+  );
+
 });
 
 /**
- * Tests failing to call the load command correctly.
+ * Tests failing to call the load command correctly, and mode change (multiple mode changes)
  */
 test("load error", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("mode");
+  await page.getByRole("button").click();
+  await(
+    expect(page.getByTestId("output")).toHaveText(
+      "Command:mode Result:Mode changed!Enter a command: Submitted 1 timesCurrent mode: verbose"
+    )
+  );
+
   await page
     .getByPlaceholder("Enter command here!")
     .fill("load poke.csv");
   await page.getByRole("button").click();
   await(
-    expect(page.getByTestId("output")).toHaveText(
-      "Command not recognized. Recognized commands include 'mode', 'view', " +
-        "'load <filepath>', and 'search <optional column identifier> <value>'Enter a command: Submitted 1 timesCurrent mode: brief"
+    expect(page.getByTestId("output")).toHaveText("Command:mode Result:Mode changed!"+
+    "Command:load poke.csv Result:Command not recognized. Recognized commands include 'mode', 'view', 'load <filepath>', " + 
+    "and 'search <optional column identifier> <value>'Enter a command: Submitted 2 timesCurrent mode: verbose"
     )
   );
   await(
     expect(page.getByTestId("output")).not.toContainText(
       "poke.csv loaded successfully!"
     )
+  );
+  await page.getByPlaceholder("Enter command here!").fill("mode");
+  await page.getByRole("button").click();
+  await(
+    expect(page.getByTestId("output")).toHaveText("Mode changed!Command not recognized. " + 
+    "Recognized commands include 'mode', 'view', 'load <filepath>', and 'search <optional column identifier> <value>'" +
+    "Mode changed!Enter a command: Submitted 3 timesCurrent mode: brief")
   );
 });
 
@@ -93,21 +125,21 @@ test("load file doesn't exist", async ({ page }) => {
   );
 });
 
-// /**
-//  * Loads multiple csv files successfully
-//  */
-// test("load multiple csv with errors", async ({ page }) => {
-//   await page.getByPlaceholder("Enter command here!").fill("load_file pizzazz.csv");
-//   await page.getByRole("button").click();
-//   await expect(page.getByTestId("output")).toHaveText(
-//     "poke.csv loaded successfully!Enter a command: Submitted 1 timesCurrent mode: brief"
-//   );
+/**
+ * Fails to load a file, then successfully loads a file
+ */
+test("load multiple csv with errors", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load_file pizzazz.csv");
+  await page.getByRole("button").click();
+  await expect(page.getByTestId("output")).toHaveText(
+    "Error: Failed to load pizzazz.csv. File doesn't exist.Enter a command: Submitted 1 timesCurrent mode: brief"
+  );
 
-//   await page.getByPlaceholder("Enter command here!").fill("load_file pizza.csv");
-//   await page.getByRole("button").click();
-//   await expect(page.getByTestId("output")).toHaveText(
-//     "poke.csv loaded successfully!pizza.csv loaded successfully!Enter a command: Submitted 2 timesCurrent mode: brief"
-//   );
-// });
+  await page.getByPlaceholder("Enter command here!").fill("load_file pizza.csv");
+  await page.getByRole("button").click();
+  await expect(page.getByTestId("output")).toHaveText(
+    "Error: Failed to load pizzazz.csv. File doesn't exist.pizza.csv loaded successfully!Enter a command: Submitted 2 timesCurrent mode: brief"
+  );
+});
 
 
